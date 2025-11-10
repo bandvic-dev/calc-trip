66c07b85d2fb3b81ed505f629233bfc086c428a5
Andrei B <working.web31@gmail.com>
2025-11-10 08:29:06 +0200

    add copyright

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <v-footer class="text-center" color="transparent" app>
+      <span class="copyright">
+        © {{ new Date().getFullYear() }} 
+        <a href="https://bandvic.vercel.app/" target="_blank" class="copyright-link">Andrei Balanco</a>
+      </span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -18,5 +24,16 @@
 body {
   background-color: #121212;
   color: rgba(255, 255, 255, 0.87);
+}
+
+.copyright {
+  width: 100%;
+  font-size: 0.9rem;
+  opacity: 0.7;
+}
+
+.copyright-link {
+  color: #4caf50;
+  text-decoration: none;
 }
 </style>
