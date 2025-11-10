71c417bd506b988e51c56beb8c943e8f44a19df7
Andrei B <working.web31@gmail.com>
2025-11-10 07:59:15 +0200

    make like app

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: false,
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
   ],
+  server: {
+    port: 3000,
+    open: true
+  },
   build: {
-    rollupOptions: {
-      input: './index.html'
-    }
+    outDir: 'dist',
+    assetsDir: 'assets'
   },
   resolve: {
     alias: {
